bf8f4447a9597c05f71b0b2a6b5c0d7986395cba
Johan Rangardt <johan.rangardt@gmail.com>
2015-10-03 19:07:07 +0200

    client setup

---
+++ client/src/app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name deffcomp2App
+ * @description
+ * # deffcomp2App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('deffcomp2App', [
+    'ngAnimate',
+    'ngResource'
+  ]);
